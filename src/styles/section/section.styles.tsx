fb165b902a8c89d1b5a0b598e5c9c7800371ca2b
0350365 <junchao.yang@mappedin.ca>
2025-03-22 02:15:11 -0400

    section styles into css

--- src/styles/section/section.styles.tsx
+++ src/styles/section/section.styles.tsx
@@ -14,7 +14,10 @@ export const SectionContentWrapper = ({
 }: SectionContentWrapperProps) => {
   return (
     <section
-      className={cn("relative pl-40 m-0 text-white box-border", className)}
+      className={cn(
+        "relative h-[100vh] w-full md:pl-[10%] m-0 text-white box-border",
+        className
+      )}
       id={id}
     >
       {children}
@@ -24,7 +27,7 @@ export const SectionContentWrapper = ({
 
 export const SectionHeader = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="relative font-cms text-2xl font-medium decoration-[2%] top-6">
+    <div className="sticky font-cms text-2xl font-medium decoration-[2%] top-0 z-10">
       {children}
     </div>
   );
